b4d37c9b712ea56052213158f87388e7dd714766
ParkGeongi <pgg113579@naver.com>
2023-01-05 17:20:03 +0900

    2023/01/05

---
+++ FastApi/docker/mysql/initdb.d/schema.sql
@@ -0,0 +1,12 @@
+create table users(
+    users_id varchar(20) primary key,
+    user_email varchar(20),
+    password varchar(20),
+    user_name varchar(20),
+    phone varchar(20),
+    birth varchar(20),
+    address varchar(20),
+    job varchar(20),
+    user_interests varchar(20),
+    token varchar(20)
+)charset = utf8;
---
+++ FastApi/docker/mysql/initdb.d/testdata.sql
@@ -0,0 +1,9 @@
+insert into users(users_id, user_email, password, user_name, phone, birth,
+                address, job, user_interests)
+values ('1', 'hong@test.com', '1', '장순이', '010-1224-5678', '2000-01-01',
+                '서울시 도봉구', '개발자', '영화');
+
+insert into users(users_id, user_email, password, user_name, phone, birth,
+                address, job, user_interests)
+values ('2', 'mang@test.com', '1', '마아니', '010-8462-1568', '2002-08-16',
+                '서울시 강남구', '사업가', '게임');
